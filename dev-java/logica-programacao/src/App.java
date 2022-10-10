654e9f6ee0eef67911b81ab622fe001463d973d2
vandersonbfs <vanderson_b@hotmail.com>
2022-10-10 20:19:28 -0300

    Exemplo IO, entrada e sáida.

---
+++ dev-java/logica-programacao/src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int n1 = ler.nextInt();
+
+        System.out.println("Digite outro número:");
+        int n2 = ler.nextInt();
+
+        int r = n1 + n2;
+
+        System.out.println("O resultado foi " + r);5
+        
+
+    }
+}
