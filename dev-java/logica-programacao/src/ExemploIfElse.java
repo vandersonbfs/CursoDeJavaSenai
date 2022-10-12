a13251cfb62fe1f9e82236fd04f8147d573634e3
vandersonbfs <vanderson_b@hotmail.com>
2022-10-11 21:01:27 -0300

    Exemplo If Else

---
+++ dev-java/logica-programacao/src/ExemploIfElse.java
@@ -0,0 +1,10 @@
+import java.net.SocketPermission;
+
+public class ExemploIfElse {
+    public static void main(String[] args) {
+        boolean x = true;
+        if (x) {
+            System.out.println("Essa menssagem é mostrada se o x for verdadeiro!");
+        }
+    }
+}
