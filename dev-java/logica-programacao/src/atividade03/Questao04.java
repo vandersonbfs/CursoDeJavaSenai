6b3cd96fb1b3fd8869a4ff7982c655d27e25d935
vandersonbfs <vanderson_b@hotmail.com>
2022-10-21 22:10:36 -0300

    Questao04

---
+++ dev-java/logica-programacao/src/atividade03/Questao04.java
@@ -0,0 +1,36 @@
+package atividade03;
+
+import java.util.Scanner;
+
+public class Questao04 {
+    public static void main(String[] args) {
+        // 4. Escreva um programa que vai ler uma palavra, se a palavra for diferente de
+        // "Java-2022" o retorno vai ser "Palavra Errada" se igual "Palavra Ok" a pessoa
+        // pode faze no máximo 5 tentativas de acertar a palavra.
+
+        int contar = 0;
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite uma mensagem");
+        String men = ler.nextLine();
+        String senha = "Java-2022";
+
+        if (men == senha) {
+
+            System.out.println("Acertou");
+            contar = 5;
+
+        }
+
+        do {
+
+            contar++;
+            System.out.println("Tentativa número: " + contar + "men: " + men + " , senha: " + senha);
+            System.out.println("Tente novamente");
+            men = ler.nextLine();
+
+        } while (contar != 5);
+        ler.close();
+    }
+}
