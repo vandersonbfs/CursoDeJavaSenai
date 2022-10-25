9b73dcbba85b6ea97eef5b6054f8c2c5acc2dfa1
vandersonbfs <vanderson_b@hotmail.com>
2022-10-25 19:47:04 -0300

    ExemploArray

---
+++ dev-java/logica-programacao/src/ExemploArray.java
@@ -0,0 +1,20 @@
+public class ExemploArray {
+    public static void main(String[] args) {
+        // array inicia da posição 0
+
+        String[] pessoas = { "Chaves", "Seu Madruga", "Dona Florinda", "Chiquinha" };
+        System.out.println(pessoas[2]);
+        System.out.println(pessoas.length);
+        for (int i = 0; i < pessoas.length; i++) {
+            String pessoa = pessoas[i];
+
+            System.out.println(pessoa);
+
+        }
+
+        for (String pessoa : pessoas) {
+            System.out.println(pessoa);
+        }
+
+    }
+}
