cf83481f889820d8d422ce3753a86765a5044d7d
vandersonbfs <vanderson_b@hotmail.com>
2022-10-25 21:32:07 -0300

    Exemplo02

---
+++ dev-java/logica-programacao/src/ExemploArray/Exemplo02.java
@@ -0,0 +1,21 @@
+package ExemploArray;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+
+        for (int i = 0; i < numeros.length; i++) {
+            String temp = JOptionPane.showInputDialog("Digite um numero!");
+            numeros[i] = Integer.parseInt(temp);
+        }
+
+        String numerosDigitados = "";
+
+        for (int i : numeros) {
+            // System.out.println(i);
+            // numerosDigitados = numerosDigitados + "," + i; equivalente
+            numerosDigitados += "," + i;
+        }
+        JOptionPane.showMessageDialog(null, numerosDigitados);
+    }
+}
