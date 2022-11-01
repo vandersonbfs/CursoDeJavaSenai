b292b2a1089c75cdc2ac11ddf19895a81bd7ef7b
vandersonbfs <vanderson_b@hotmail.com>
2022-11-01 20:12:16 -0300

    Exemplo00

---
+++ dev-java/logica-programacao/src/ExemploArray/ExemplosPoo/Exemplo00.java
@@ -0,0 +1,18 @@
+package ExemploArray.ExemplosPoo;
+
+public class Exemplo00 {
+    public static void main(String[] args) {
+        Caneta caneta1 = new Caneta("BIC","Azul");
+        caneta1.printStatus();
+        caneta1.rabiscar();
+        caneta1.printStatus();
+        caneta1.destampar();
+        caneta1.rabiscar();
+        caneta1.printStatus();
+        caneta1.escrever("Luiz Felipe Freire Maia");
+        caneta1.printStatus();
+        caneta1.escrever("Luiz");
+        caneta1.printStatus();
+        Caneta caneta2 = new Caneta("BIC", "Preta");
+        caneta2.printStatus();
+}
