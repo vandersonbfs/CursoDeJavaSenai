bc738f1a4bc11832741e0f492d7239b1c14f451a
vandersonbfs <vanderson_b@hotmail.com>
2022-11-15 20:34:29 -0300

    Correção, add @RestController

--- dev-java/logica-programacao/src/prj-java/src/main/java/org/senai/prjjava/PrjJavaApplication.java
+++ dev-java/logica-programacao/src/prj-java/src/main/java/org/senai/prjjava/PrjJavaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PrjJavaApplication {
 
 	public static void main(String[] args) {
