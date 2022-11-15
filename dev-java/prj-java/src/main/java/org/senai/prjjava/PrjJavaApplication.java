514dcb15f320b8d74556d9c1fd6f761b3120e48a
vandersonbfs <vanderson_b@hotmail.com>
2022-11-15 20:03:54 -0300

    prj-java

---
+++ dev-java/prj-java/src/main/java/org/senai/prjjava/PrjJavaApplication.java
@@ -0,0 +1,13 @@
+package org.senai.prjjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjJavaApplication.class, args);
+	}
+
+}
---
+++ dev-java/prj-java/src/test/java/org/senai/prjjava/PrjJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.prjjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
